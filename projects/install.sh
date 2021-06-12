8d1d7344e6716e2c418281cb3c6d2c0f34e6887c
Raymond <raymond@hetwebbureau.nl>
2021-06-12 21:11:40 +0200

    added comments

--- projects/install.sh
+++ projects/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# source / execute in each project ./install.sh
 for dir in $DOTFILES_PROJECTS_DIR/*
 do
   test -d "$dir" || continue
-  chmod +x $dir/install.sh
   source $dir/install.sh
 done
