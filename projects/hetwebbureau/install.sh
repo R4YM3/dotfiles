5a8ab0a3713752d4f15c9ae7a6196bceb95abc6d
Raymond <raymond@hetwebbureau.nl>
2021-06-24 22:49:07 +0200

    feat: renamed web-cli-interpeter to web-cli-shell

--- projects/hetwebbureau/install.sh
+++ projects/hetwebbureau/install.sh
@@ -15,8 +15,8 @@ install_tmuxinator_project "$HETWEBBUREAU_DIR/react-cli/tmuxinator.yml" "react-c
 git clone https://github.com/R4YM3/web-cli.git "$HETWEBBUREAU_DIR/web-cli"
 install_tmuxinator_project "$HETWEBBUREAU_DIR/web-cli/tmuxinator.yml" "web-cli.yml"
 
-git clone https://github.com/R4YM3/web-cli-interpreter.git "$HETWEBBUREAU_DIR/web-cli-interpreter"
-install_tmuxinator_project "$HETWEBBUREAU_DIR/web-cli-interpreter/tmuxinator.yml" "web-cli-interpreter.yml"
+git clone https://github.com/R4YM3/web-cli-shell.git "$HETWEBBUREAU_DIR/web-cli-shell"
+install_tmuxinator_project "$HETWEBBUREAU_DIR/web-cli-shell/tmuxinator.yml" "web-cli-shell.yml"
 
 git clone git@bitbucket.org:R4YM3/private-repositories-installer.git "$HETWEBBUREAU_DIR/private-repositories-installer"
 install_tmuxinator_project "$HETWEBBUREAU_DIR/private-repositories-installer/tmuxinator.yml" "private-repositories-installer.yml"
