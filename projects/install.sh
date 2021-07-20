6249daedc51099fd5282938f0c13a63132440c90
Raymond <raymond@hetwebbureau.nl>
2021-07-20 15:59:44 +0200

    projects: pull changes

--- projects/install.sh
+++ projects/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 download_repository git@bitbucket.org:R4YM3/private-repositories-installer.git $PROJECTS_DIR
+cd $PROJECTS_DIR && git pull
 
 # source .install.sh from each project
 for dir in $PROJECTS_DIR/*
