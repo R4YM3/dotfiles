841deb22417278fdee88ba30dc709bdc797a7679
Raymond Schweers <raymond@hetwebbureau.nl>
2021-08-02 18:06:05 +0200

    added dotfiles-ansible

--- projects/hetwebbureau/install.sh
+++ projects/hetwebbureau/install.sh
@@ -9,6 +9,9 @@ mkdir -p $HETWEBBUREAU_DIR
 git clone https://github.com/R4YM3/dotfiles.git $HETWEBBUREAU_DIR/dotfiles
 install_tmuxinator_project "$HETWEBBUREAU_DIR/dotfiles/tmuxinator.yml" "dotfiles.yml"
 
+git clone https://github.com/R4YM3/dotfiles-ansible.git $HETWEBBUREAU_DIR/dotfiles-ansible
+install_tmuxinator_project "$HETWEBBUREAU_DIR/dotfiles-ansible/tmuxinator.yml" "dotfiles-ansible.yml"
+
 git clone https://github.com/R4YM3/react-cli.git "$HETWEBBUREAU_DIR/react-cli"
 install_tmuxinator_project "$HETWEBBUREAU_DIR/react-cli/tmuxinator.yml" "react-cli.yml"
 
